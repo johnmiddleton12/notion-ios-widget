fa5fde6ab7460c41c564236feeb36e5ec08e504b
Johnathan <johnathanmiddleton1@gmail.com>
2022-12-23 16:33:31 -0500

    testing notion api

---
+++ gists/NotionWidget.js
@@ -0,0 +1,38 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: magic;
+
+const { NOTION_KEY, NOTION_DATABASE_ID } = require('./env/config.js');
+
+console.log(NOTION_KEY, NOTION_DATABASE_ID);
+
+let message = 'Helloooo Woooooorld!'
+let param = config.widgetParameter
+if (param != null && param.length > 0) {
+    message = param
+}
+
+const widget = new ListWidget()
+
+widget.addSpacer(4)
+
+widget.setPadding(10, 10, 10, 10)
+
+let row = widget.addStack()
+row.layoutHorizontally()
+
+let column = row.addStack()
+column.layoutVertically()
+
+column.addText(message)
+column.addText("Version: development")
+
+let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
+
+column.addText(currentTime)
+
+if (config.runsInWidget) {
+    Script.setWidget(widget)
+} else {
+    widget.presentSmall()
+}
---
+++ testNotionApi.js
@@ -0,0 +1,49 @@
+const { NOTION_KEY, NOTION_DATABASE_ID } = require('./env/config.js');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+var request = require("request");
+var options = {
+    method: "POST",
+    url: `https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`,
+    headers: {
+        Authorization: `Bearer ${NOTION_KEY}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-02-22",
+    },
+    body: JSON.stringify({
+        filter: {
+                "property": "Tags",
+                "multi_select": {
+                    contains: "WORK",
+                },
+        },
+        // sort by time created
+        sorts: [
+            {
+                timestamp: "created_time",
+                direction: "descending",
+            },
+        ],
+
+    }),
+};
+request(options, function (error, response) {
+    if (error) throw new Error(error);
+    // console.log(response.body);
+
+    let data = JSON.parse(response.body);
+    let results = data.results;
+
+    // console.log(results);
+
+    for (let i = 0; i < results.length; i++) {
+        let page = results[i];
+
+        console.log(page.properties['Task name'].title[0].plain_text);
+        console.log(page.url);
+
+    }
+
+});
