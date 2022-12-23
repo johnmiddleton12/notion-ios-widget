cc9c6977085c6dc712f3da04314cc694f54f609d
Johnathan <johnathanmiddleton1@gmail.com>
2022-12-23 13:29:32 -0500

    1

--- gists/ScriptableWidget.js
+++ gists/ScriptableWidget.js
@@ -2,45 +2,33 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: magic;
 
-//@ts-check
-
-/**
- * Create the widget
- * @param {{widgetParameter: string, debug: string}} config widget configuration
- */
-async function createWidget(config) {
-    const log = config.debug ? console.log.bind(console) : function () {};
-    log(JSON.stringify(config, null, 2))
-
-    let message = 'Helloooo Woooooorld!'
-    let param = config.widgetParameter
-    if (param != null && param.length > 0) {
-        message = param
-    }
-
-    // @ts-ignore
-    const widget = new ListWidget()
-
-    widget.addSpacer(4)
-
-    widget.setPadding(10, 10, 10, 10)
-
-    let row = widget.addStack()
-    row.layoutHorizontally()
-
-    let column = row.addStack()
-    column.layoutVertically()
-
-    column.addText(message)
-    column.addText("Version: development")
-
-    let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
-
-    column.addText(currentTime)
-
-    return widget
+let message = 'Helloooo Woooooorld!'
+let param = config.widgetParameter
+if (param != null && param.length > 0) {
+    message = param
 }
 
-module.exports = {
-    createWidget
+const widget = new ListWidget()
+
+widget.addSpacer(4)
+
+widget.setPadding(10, 10, 10, 10)
+
+let row = widget.addStack()
+row.layoutHorizontally()
+
+let column = row.addStack()
+column.layoutVertically()
+
+column.addText(message)
+column.addText("Version: development")
+
+let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
+
+column.addText(currentTime)
+
+if (config.runsInWidget) {
+    Script.setWidget(widget)
+} else {
+    widget.presentSmall()
 }
