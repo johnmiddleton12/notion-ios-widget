ada8a276bce3dd28c18831422d3952272488beb7
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-12-27 21:28:42 -0500

    setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+cd gists
+
+git clone https://gist.github.com/johnmiddleton12/c15ecdc2826d105c04f93aa05facc136
+
+cp -r c15ecdc2826d105c04f93aa05facc136/.git ./
+
+rm -rf c15ecdc2826d105c04f93aa05facc136/
+
+cd ../
