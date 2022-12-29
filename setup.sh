f9a3409658838baff5a83fcf3a7e98e4368fe44e
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-12-28 22:39:15 -0500

    modify setup to replace gists with gists repo

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ cd gists
 
 git clone https://gist.github.com/johnmiddleton12/c15ecdc2826d105c04f93aa05facc136
 
-cp -r c15ecdc2826d105c04f93aa05facc136/.git ./
+cp -rf c15ecdc2826d105c04f93aa05facc136/* ./
 
 rm -rf c15ecdc2826d105c04f93aa05facc136/
 
